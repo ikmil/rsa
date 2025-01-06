208e340d40746d32fa5fef848455d5f5e27e6c41
yzq <464093516@qq.com>
2025-01-06 18:20:51 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2024-12-25 19:41:01
+ * @LastEditors: your name
+ * @LastEditTime: 2025-01-06 15:31:07
+ * @Description: 
+ * @FilePath: /rsa/nuxt.config.ts
+ */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  typescript:{typeCheck:true}
 })
